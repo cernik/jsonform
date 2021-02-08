89228917b7890f5ac6c1cd350525c8393c7e5b5d
agevinto <agevinto@isracard.co.il>
2021-02-08 18:25:44 +0200

    Set minimal sample

--- App.js
+++ App.js
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   SafeAreaView,
@@ -14,61 +7,117 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
+  Button,
+  LayoutAnimation,
 } from 'react-native';
+import {TextField} from 'react-native-material-textfield';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Formik} from 'formik';
+
+import jsonform from './samples/sample.json';
 
 const App: () => React$Node = () => {
+  const renderField = (field, props) => {
+    const {handleChange, handleBlur, setFieldValue, values} = props;
+
+    if (field.subtype === 'checkbox') {
+      if (!field.enabled) {
+        return null;
+      }
+      return (
+        <View key={field.id} style={{marginTop: 16}}>
+          <Text style={{marginStart: 4}}>{field.label || ''}</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              LayoutAnimation.configureNext(
+                LayoutAnimation.Presets.easeInEaseOut,
+              );
+              setFieldValue(field.id, !values[field.id]);
+            }}
+            style={{
+              margin: 4,
+              height: 20,
+              width: 20,
+              borderWidth: 1,
+            }}>
+            {values[field.id] && (
+              <View
+                style={{
+                  margin: 3,
+                  backgroundColor: 'grey',
+                  flex: 1,
+                }}
+              />
+            )}
+          </TouchableOpacity>
+        </View>
+      );
+    }
+    let keyboardType = 'default';
+    if (field.subtype === 'number') {
+      keyboardType = 'number-pad';
+    } else if (field.subtype === 'email') {
+      keyboardType = 'email-address';
+    }
+    if (field.enabled === false) {
+      return null;
+    }
+
+    if (typeof field.depenedOn === 'object' && Array.isArray(field.depenedOn)) {
+      const [parentFieldId, parentFieldValue] = field.depenedOn;
+      if (values[parentFieldId] !== parentFieldValue) {
+        return null;
+      }
+    }
+    return (
+      <View key={field.id}>
+        <TextField
+          label={field.label}
+          keyboardType={keyboardType}
+          onChangeText={handleChange(field.id)}
+          onBlur={handleBlur(field.id)}
+          value={String(values[field.id])}
+          placeholder={field.placeholder}
+        />
+      </View>
+    );
+  };
+
   return (
-    <>
+    <React.Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
+          <Header style={{height: 80}} />
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+              <Formik
+                enableReinitialize
+                initialValues={jsonform.fields.reduce(
+                  (acc, f) => ({
+                    ...acc,
+                    [f.id]: f.value,
+                  }),
+                  {},
+                )}
+                onSubmit={(values) => alert(JSON.stringify(values, null, 2))}>
+                {(props) => (
+                  <React.Fragment>
+                    {jsonform.fields.map((field) => renderField(field, props))}
+                    <Button onPress={props.handleSubmit} title="Submit" />
+                  </React.Fragment>
+                )}
+              </Formik>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
-    </>
+    </React.Fragment>
   );
 };
 
@@ -76,38 +125,12 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
   },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
